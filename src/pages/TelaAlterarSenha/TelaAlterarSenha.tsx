6f010fc3700d5afd4a627b9138be449a584bd708
PedroBaffa <pedro2004baffa@gmail.com>
2025-11-12 09:21:26 -0300

    Importação e Rederização do último step.

--- src/pages/TelaAlterarSenha/TelaAlterarSenha.tsx
+++ src/pages/TelaAlterarSenha/TelaAlterarSenha.tsx
@@ -3,7 +3,8 @@ import styles from "./TelaAlterarSenha.module.css";
 import { HeroLogo } from "../../components/HeroLogo/HeroLogo";
 import { Step1_Senha } from "./components/Step1_Senha";
 import { Step2_Preferencias } from "./components/Step2_Preferencias";
-import { Step3_Unidades } from "./components/Step3_Unidades"; // Importação do Step3
+import { Step3_Unidades } from "./components/Step3_Unidades";
+import { Step4_Finalizar } from "./components/Step4_Finalizar";
 
 export function TelaAlterarSenha() {
   const [step, setStep] = useState(1);
@@ -18,20 +19,19 @@ export function TelaAlterarSenha() {
         <div className={styles.logoWrapper}>
           <HeroLogo />
         </div>
-        {step === 1 && (
+        {step === 1 && ( // Renderização do Step1
           <Step1_Senha styles={styles} onContinue={handleNextStep} />
         )}
 
-        {step === 2 && (
-          <Step2_Preferencias
-            styles={styles}
-            onContinue={handleNextStep}
-          />
+        {step === 2 && ( // Renderização do Step2
+          <Step2_Preferencias styles={styles} onContinue={handleNextStep} />
         )}
 
         {step === 3 && ( // Renderização do Step3
-          <Step3_Unidades styles={styles} />
+          <Step3_Unidades styles={styles} onContinue={handleNextStep} />
         )}
+
+        {step === 4 && <Step4_Finalizar styles={styles} />}
       </div>
 
       <div className={styles.bottomBars}>
