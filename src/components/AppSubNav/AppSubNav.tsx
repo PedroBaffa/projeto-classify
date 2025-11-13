c37a3f99d53ff4ce8aa012a68120195fd5cff622
PedroBaffa <pedro2004baffa@gmail.com>
2025-11-13 15:16:15 -0300

    Adição e estilização do SubNav, navegação das páginas do dashboard.

---
+++ src/components/AppSubNav/AppSubNav.tsx
@@ -0,0 +1,20 @@
+// src/components/AppSubNav/AppSubNav.tsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './AppSubNav.module.css';
+
+export function AppSubNav() {
+  
+  const getLinkClass = ({ isActive }: { isActive: boolean }) => {
+    return isActive ? `${styles.navLink} ${styles.active}` : styles.navLink;
+  };
+
+  return (
+    <nav className={styles.appSubNav}>
+      <NavLink to="/dashboard" className={getLinkClass}>Quadro de Aulas</NavLink>
+      <NavLink to="/minhas-ucs" className={getLinkClass}>Minhas UCs</NavLink>
+      <NavLink to="/relatorios" className={getLinkClass}>Relatórios</NavLink>
+      <NavLink to="/solicitacoes" className={getLinkClass}>Solicitações</NavLink>
+    </nav>
+  );
+}
