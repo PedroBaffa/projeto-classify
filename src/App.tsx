684f008b7efedc9f2956c4bff743674784eb8c30
PedroBaffa <pedro2004baffa@gmail.com>
2025-11-13 14:54:41 -0300

    Importação e rotas da nova página de dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,33 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./components/Layout/Layout";
-import { TelaInicial } from "./pages/TelaInicial/TelaInicial";
-import { TelaAtuacao } from "./pages/TelaAtuacao/TelaAtuacao";
-import { TelaServicos } from "./pages/TelaServicos/TelaServicos";
-import { TelaSuporte } from "./pages/TelaSuporte/TelaSuporte";
-import { TelaSobreNos } from "./pages/TelaSobreNos/TelaSobreNos";
-import { TelaLogin } from "./pages/TelaLogin/TelaLogin";
-import { TelaCadastro } from "./pages/TelaCadastro/TelaCadastro";
-import { TelaAlterarSenha } from "./pages/TelaAlterarSenha/TelaAlterarSenha";
-
-function PaginaDashboard() { // apagar após implementar a dashboard
-  return (
-    <main
-      style={{
-        padding: "2rem 20px",
-        zIndex: 1,
-        maxWidth: "960px",
-        margin: "0 auto",
-      }}
-    >
-      <h1>Dashboard (Em Breve)</h1>
-    </main>
-  );
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { DashboardLayout } from './components/DashboardLayout/DashboardLayout';
+import { TelaInicial } from './pages/TelaInicial/TelaInicial';
+import { TelaAtuacao } from './pages/TelaAtuacao/TelaAtuacao'; 
+import { TelaSobreNos } from './pages/TelaSobreNos/TelaSobreNos';
+import { TelaLogin } from './pages/TelaLogin/TelaLogin';
+import { TelaCadastro } from './pages/TelaCadastro/TelaCadastro';
+import { TelaAlterarSenha } from './pages/TelaAlterarSenha/TelaAlterarSenha';
+import { QuadroDeAulas } from './pages/QuadroDeAulas/QuadroDeAulas';
 
 function App() {
   return (
     <Routes>
+      
       <Route element={<Layout />}>
         <Route path="/" element={<TelaInicial />} />
         <Route path="/atuacao" element={<TelaAtuacao />} />
-        <Route path="/servicos" element={<TelaServicos />} />
-        <Route path="/suporte" element={<TelaSuporte />} />
         <Route path="/sobre-nos" element={<TelaSobreNos />} />
       </Route>
-
+      
       <Route path="/entrar" element={<TelaLogin />} />
-      <Route path="/cadastro" element={<TelaCadastro />} />
-      <Route path="/primeiro-login" element={<TelaAlterarSenha />} />
+      <Route path="/cadastro" element={<TelaCadastro />} /> 
+      <Route path="/primeiro-login" element={<TelaAlterarSenha />} /> 
 
-      <Route path="/dashboard" element={<PaginaDashboard />} />
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<QuadroDeAulas />} />
+      </Route>
+      
     </Routes>
   );
 }
