3d1bd90d90d9271509da93f5636efd67b5a7639c
PedroBaffa <pedro2004baffa@gmail.com>
2025-11-13 16:02:01 -0300

    Criação, alteração e reestruturação da exibição do quadro de aulas.

--- src/pages/QuadroDeAulas/QuadroDeAulas.tsx
+++ src/pages/QuadroDeAulas/QuadroDeAulas.tsx
@@ -1,11 +1,32 @@
+// src/pages/QuadroDeAulas/QuadroDeAulas.tsx
 import React from 'react';
 import styles from './QuadroDeAulas.module.css';
 
 export function QuadroDeAulas() {
   return (
-    <div className={styles.page}>
-      <h1>Quadro de Aulas (Em Breve)</h1>
-      <p>O grid da semana virá aqui.</p>
-    </div>
+    <>
+      <div className={styles.filterBar}>
+        
+        <select className={styles.dropdownMes}>
+          <option>Mês</option>
+        </select>
+
+        <div className={styles.weekControl}>
+          {/* --- MUDANÇA AQUI --- */}
+          <button className={styles.weekArrow}>{"‹"}</button> 
+          <span className={styles.weekLabel}>Semana 01</span>
+          <button className={styles.weekArrow}>{"›"}</button>
+          {/* --- FIM DA MUDANÇA --- */}
+        </div>
+
+        <button className={styles.todayButton}>
+          Exibir hoje
+        </button>
+      </div>
+
+      <div className={styles.weekGrid}>
+        <p>(arrumando grid de quadro de aulas)</p>
+      </div>
+    </>
   );
 }
