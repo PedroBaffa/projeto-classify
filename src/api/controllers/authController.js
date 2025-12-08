6f4a6ce912bea302c6b8ef793800e6bb50cbc06b
PedroBaffa <pedro2004baffa@gmail.com>
2025-12-08 19:18:25 -0300

    alteração na pasta de api com criação de controlador e rotas

---
+++ src/api/controllers/authController.js
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+const SECRET = "SEGREDO_SUPER_SECRETO"; // em produção
+
+export const login = async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Usuário não encontrado." });
+    }
+
+    const senhaValida = await bcrypt.compare(senha, user.senha);
+
+    if (!senhaValida) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Senha incorreta." });
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {
+      expiresIn: "1d",
+    });
+    const { senha: _, ...userSemSenha } = user;
+
+    res.json({ success: true, user: userSemSenha, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Erro no servidor" });
+  }
+};
+
+export const register = async (req, res) => {
+  res.json({ message: "Rota de registro a implementar" });
+};
